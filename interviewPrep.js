4f9eeca76f30497e3986342e2fe7deaf879fd78d
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-05-10 01:32:01 -0400

    randomize which question is displayed in interview-prep.html

--- interviewPrep.js
+++ interviewPrep.js
@@ -1,9 +1,27 @@
 let quiz = 
 [
     {
-    q:`what is the HTML syntax to connect a css file?<br><br>
-        <input type="text">
+    q:`what is the HTML syntax to connect a css file?
+    <br><br><br><br><br><br><br><br><br><br>
+       <input type="text" align="middle">
     `,
-    a:'<link rel="stylesheet" href="style.css">'
-    }
+    a:`&lt;link rel="stylesheet" href="style.css">`
+    },
+
+    {
+    q:`what is the HTML syntax to connect a css file?
+    <br><br><br><br><br><br><br><br><br><br>
+       <input type="text" align="middle">
+        `,
+    a:`&lt;link rel="stylesheet" href="style.css">`
+    },
+
+    {
+    q:`what is the HTML syntax to connect a css file?
+    <br><br><br><br><br><br><br><br><br><br>
+       <input type="text" align="middle">
+    `,
+    a:`&lt;link rel="stylesheet" href="style.css">`
+    },
+                
 ]
