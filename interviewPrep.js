5b424e52b2fc52e859587c923facb88f0d02b558
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-05-31 14:38:52 -0400

    add questions in interviewPrep.js

--- interviewPrep.js
+++ interviewPrep.js
@@ -311,7 +311,21 @@ let quiz =
         </div>`,
     
         a:`function DECLARATION<br><br>function x(){}`
-    }
+    },
+
+    {
+        q:`which is a function DECLARATION?<br><br>
+        <div>
+        <input type='radio' id='two' name='declaration'>
+        <label for='two'>const x = function(){}</label>
+        </div><br>
+        <div>
+        <input type='radio' id='one' name='declaration'>
+        <label for='one'>function x(){}</label>
+        </div>`,
+    
+        a:`function DECLARATION<br><br>function x(){}`
+    }    
     
 
 
