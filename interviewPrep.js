16a4daca919016b85a75b3966afc35d226e716ec
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-29 10:26:23 -0400

    add question to interviewPrep.js

--- interviewPrep.js
+++ interviewPrep.js
@@ -443,6 +443,13 @@ let quiz =
     {
         q:`slice() vs splice<br>which modifies original array?`,
         a:`splice()<br>modifies original array`
+    },
+
+    {
+        q:`arr=[1,2,3]<br><br>
+           add 0 to the beginning of the array<br><br>
+           <input type=text>`,
+        a:`arr.unshift(0)`
     }
     
     
--- test.js
+++ test.js
@@ -2,17 +2,10 @@ let quiz =
 [
 
 {
-    q:`which is a function DECLARATION?<br><br>
-    <div>
-    <input type='radio' id='one' name='declaration'>
-    <label for='one'>function x(){}</label>
-    </div><br>
-    <div>
-    <input type='radio' id='two' name='declaration'>
-    <label for='two'>const x = function(){}</label>
-    </div>`,
-
-    a:`function DECLARATION<br><br>function x(){}`
+    q:`arr=[1,2,3]<br><br>
+    add 0 to the beginning of the array<br><br>
+    <input type=text>`,
+ a:`arr.unshift(0)`
 }
 
 
