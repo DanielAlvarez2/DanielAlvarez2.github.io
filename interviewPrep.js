aa9593dafb405905f870d511fdd71b03a9fa56d7
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-29 10:31:19 -0400

    add question to interviewPrep.js

--- interviewPrep.js
+++ interviewPrep.js
@@ -449,7 +449,16 @@ let quiz =
         q:`arr=[1,2,3]<br><br>
            add 0 to the beginning of the array<br><br>
            <input type=text>`,
-        a:`arr.unshift(0)`
+        a:`arr.unshift(0)<br><br>
+           arr=[0,1,2,3]`
+    },
+
+    {
+        q:`arr=[0,1,2,3]<br><br>
+           remove 0 from the beginning of the array<br><br>
+           <input type='text'>`,
+        a:`arr.shift()<br><br>
+           arr=[1,2,3]`
     }
     
     
--- test.js
+++ test.js
@@ -2,10 +2,12 @@ let quiz =
 [
 
 {
-    q:`arr=[1,2,3]<br><br>
-    add 0 to the beginning of the array<br><br>
-    <input type=text>`,
- a:`arr.unshift(0)`
+    
+    q:`arr=[0,1,2,3]<br><br>
+    remove 0 from the beginning of the array<br><br>
+    <input type='text'>`,
+ a:`arr.shift()<br><br>
+    arr=[1,2,3]`
 }
 
 
